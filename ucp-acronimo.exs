f545349d653ffe119b98113b11295aea0b5e12da
Facundopeloso <facundogpeloso@hotmail.com>
2017-10-04 18:45:37 -0300

    Nuevos ejemplos de le elixir

---
+++ ucp-acronimo.exs
@@ -0,0 +1,8 @@
+defmodule UCP do
+    def acronimo(x) do
+        demo = Regex.replace(~r/[a-z]/, x, "")
+        demo = Regex.replace(~r/\s/, demo, "")
+        demo = Regex.replace(~r/[áéíóúÁÉÍÓÚ]/, demo, "")
+        IO.puts "#{demo}"
+    end 
+end
---
+++ ucp-numeroPares.exs
@@ -0,0 +1,12 @@
+pares = fn(n) -> x=rem(n, 2) 
+if x ==0 do
+    IO.puts "#{n}"
+end
+end
+Enum.map [1, 2, 3, 4, 5], pares
+
+
+    
+
+
+
